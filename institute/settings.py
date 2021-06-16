0afe3220f79d0b822fda61e01a56a1710e8e5c3b
karikaran <68324875+Karikaranvetti@users.noreply.github.com>
2021-06-17 01:02:34 +0530

    Update settings.py

--- institute/settings.py
+++ institute/settings.py
@@ -142,7 +142,7 @@ MAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'happyvendingteam@gmail.com'
+EMAIL_HOST_USER = '************'
 
-EMAIL_HOST_PASSWORD = 'Vk6428987'
+EMAIL_HOST_PASSWORD = '*********'
 
